5646912cc64a4ebe220573fcf9ec313800cea2dd
Yury Kuzma <vterkin1388@gmail.com>
2024-09-19 22:56:20 +0300

    replace parse from ValueNode to ObjectConverter, add parse UUID

--- src/main/java/ru/clevertec/core/ObjectConverter.java
+++ src/main/java/ru/clevertec/core/ObjectConverter.java
@@ -5,8 +5,12 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
 
 public class ObjectConverter {
+
+    int i;
 
     public <T> T convert(Node node, Class<T> object) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
 
@@ -27,12 +31,35 @@ public class ObjectConverter {
                 field.setAccessible(true);
                 Node value = fieldNode.getValue();
                 if (value.isValue()) {
-                    field.set(t, ((ValueNode) value).getValue());
+                    Object valueNode = parse(((ValueNode) value).getValue(), field);
+                    field.set(t, valueNode);
                 }
             }
         }
 
         return t;
     }
+
+
+    private Object parse(String value, Field field) {
+        value  = value.trim();
+
+        if (field.getType().equals(String.class)) {
+            return value.substring(1, value.length() - 1);
+        } else if (field.getType().equals(boolean.class)) {
+            return Boolean.parseBoolean(value);
+        } else if (value.equals("null")) {
+            return null;
+        } else if (field.getType().equals(int.class)) {
+            return Integer.parseInt(value);
+        } else if (field.getType().equals(double.class)) {
+            return Double.parseDouble(value);
+        } else if (field.getType().equals(UUID.class)) {
+
+            return UUID.fromString(value.substring(1, value.length() - 1));
+        }
+        throw new IllegalArgumentException();
+    }
+
 
 }
--- src/main/java/ru/clevertec/core/ValueNode.java
+++ src/main/java/ru/clevertec/core/ValueNode.java
@@ -7,32 +7,14 @@ import lombok.Setter;
 @Setter
 public class ValueNode extends Node {
 
-    private Object value;
+    private String value;
 
     public ValueNode() {
         super(NodeType.VALUE);
     }
 
-    public void setValueAsString(String value) {
 
-        this.value = parse(value);
-    }
 
-    private Object parse(String value) {
-        value = value.trim();
-               
-        if (value.matches("\".*\"")) {
-            return value.substring(1, value.length() - 1);
-        } else if (value.equals("true") || value.equals("false")) {
-            return Boolean.parseBoolean(value);
-        } else if (value.equals("null")) {
-            return null;
-        } else if (value.matches("-?\\d+")) {
-            return Integer.parseInt(value);
-        } else if (value.matches("-{0,1}\\d+\\.\\d+")) {
-            return Double.parseDouble(value);
-        }
-        throw new IllegalArgumentException();
-    }
+
 
 }
