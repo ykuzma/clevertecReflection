7d540e0f4c02f5da74ad0d119dfc2f44b541523c
Yury Kuzma <vterkin1388@gmail.com>
2024-10-06 21:55:42 +0300

    refactor ConverterMapNode

--- src/main/java/ru/clevertec/core/service/deserialization/ConverterMapNode.java
+++ src/main/java/ru/clevertec/core/service/deserialization/ConverterMapNode.java
@@ -13,32 +13,34 @@ import java.util.Map;
 
 @NoArgsConstructor
 @AllArgsConstructor
-public class ConverterMapNode implements NodeConverter{
-
+public class ConverterMapNode implements NodeConverter {
     private NodeConverterFactory factory;
-
 
     @Override
     public <T> T convert(Node node, ContainerData<T> containerData) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, ClassNotFoundException {
         Class<T> containerClass = containerData.getContainerClass();
         Class<?> implementation = containerData.getContainerClassImpl();
         T map = containerClass.cast(implementation.getConstructor().newInstance());
-        Method addInContainer = containerData.getAddInContainer();
+        Method addInMap = containerData.getAddInContainer();
 
-        ObjectNode objectNode = (ObjectNode) node;
-        for (Map.Entry<String, Node> n : objectNode.getFields().entrySet()) {
-            Node nodeValue = n.getValue();
-            Type type = containerData.getTypeElementInContainer()[1];
-            Object key = parseValue(n.getKey(), (Class<?>) containerData.getTypeElementInContainer()[0]);
-            ContainerData<?> containerData1 = new ContainerData.ContainerBuilder<>(
-                    ContainerData.ContainerBuilder.extractGenericClass(type))
-                    .setTypeElementInContainer(type)
-                    .build();
-            NodeConverter nodeConverter = factory.getNodeConverter(nodeValue, containerData1);
-            Object value = nodeConverter.convert(nodeValue, containerData1);
-
-            addInContainer.invoke(map, key, value);
+        ObjectNode parentNode = (ObjectNode) node;
+        for (Map.Entry<String, Node> innerNode : parentNode.getFields().entrySet()) {
+            Class<?> keyClass  = (Class<?>)containerData.getTypeElementInContainer()[0];
+            Object key = parseValue(innerNode.getKey(), keyClass);
+            Type typeValue = containerData.getTypeElementInContainer()[1];
+            Object value = getValue(typeValue, innerNode.getValue());
+            addInMap.invoke(map, key, value);
         }
         return map;
+    }
+
+    private Object getValue(Type typeValue, Node nodeValue) throws NoSuchMethodException, ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        ContainerData<?> containerData1 = new ContainerData.ContainerBuilder<>(
+                ContainerData.ContainerBuilder.extractGenericClass(typeValue))
+                .setTypeElementInContainer(typeValue)
+                .build();
+
+        NodeConverter nodeConverter = factory.getNodeConverter(nodeValue, containerData1);
+        return nodeConverter.convert(nodeValue, containerData1);
     }
 }
